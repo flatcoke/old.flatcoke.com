ec20866f8b4a1858bc3e203d3a6bab819c1c8227
FLATCOKE <flatcoke89@gmail.com>
2019-06-16 22:31:59 +0900

    setting environemtn

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,23 @@
 const withTypescript = require('@zeit/next-typescript')
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+require('dotenv').config()
 
 module.exports = withTypescript({
   distDir: '../.next',
+  webpack: config => {
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true,
+      }),
+    ]
+    return config
+  },
 })
